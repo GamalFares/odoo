be685addf1835cd342bf1ba8336c41d3b72abde6
GamalFares <112282940+GamalFares@users.noreply.github.com>
2025-11-23 01:16:31 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export LC_ALL=C
 envsubst < /etc/odoo/odoo.conf.template > /etc/odoo/odoo.conf
-exec odoo --config=/etc/odoo/odoo.conf --http-interface=0.0.0.0 --http-port=10000
+exec odoo --config=/etc/odoo/odoo.conf --http-interface=0.0.0.0 --http-port=$PORT
