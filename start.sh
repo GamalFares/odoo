bc7a45afae2e6f21fc3808eae49fe6d02760c073
GamalFares <112282940+GamalFares@users.noreply.github.com>
2025-11-23 02:38:55 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 export LC_ALL=C
+
+# Wait for database
+echo "Waiting for database..."
+sleep 10
+
+# Substitute environment variables
 envsubst < /etc/odoo/odoo.conf.template > /etc/odoo/odoo.conf
-exec odoo --config=/etc/odoo/odoo.conf --http-interface=0.0.0.0 --http-port=$PORT
+
+# Start Odoo with asset regeneration
+exec odoo --config=/etc/odoo/odoo.conf --http-interface=0.0.0.0 --http-port=$PORT --dev=all
